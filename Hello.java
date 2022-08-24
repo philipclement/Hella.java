c18e1cc7b9f78da4a22d4fff2a1d2a36e05748df
philipclement <112023428+philipclement@users.noreply.github.com>
2022-08-24 16:19:09 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**********************************************
+* Hello.java
+***********************************************
+* Philip Clement
+* 08/24/2022
+* CSMC225
+***********************************************/
+
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Philip Clement");
+		System.out.println("Hello");
+		System.out.println("Lab 1 - Instructions PC");
+		System.out.println("CMSC225");
+		System.out.println();
+	}
+}
